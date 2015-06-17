d93fb6e612b14dc74d3f2495afe5e5dd8897f612
miguelgonz <Miguel.Gonzalez@bbc.co.uk>
2015-06-17 09:53:21 +0100

    Limit downloads with semaphores

--- main.js
+++ main.js
@@ -1,27 +1,35 @@
 var request = require('http-request');
 var nodeskull = require('./index.js');
 var readline = require('readline');
+var semaphoreFactory = require('semaphore');
+var fs = require('fs');
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   terminal: false
 });
 
+var limiter = semaphoreFactory(5);
+
 rl.on('line', function (line) {
-    nodeskull.search(line, function (link) {
-        if (!link) {
-            console.log(line + "\tNOT FOUND");
-            return;
-        }
-        console.log(line + "\t" + link);
-        request.get(
-            link,
-            line + ".mp3",
-            function (err) {
+    var path = line + ".mp3";
+    if (fs.existsSync(path)) {
+        console.log(line + " already exists");
+        return;
+    }
+    limiter.take(function () {
+        nodeskull.search(line, function (link) {
+            if (!link) {
+                console.log(line + "\tNOT FOUND");
+                return;
+            }
+            console.log(line + "\t" + link);
+            request.get(link, path, function (err) {
+                limiter.leave();
                 if (!err) {
                     console.log(line + " downloaded");
                 }
-            }
-        );
+            });
+        });
     });
 });
